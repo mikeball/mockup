ee4a236fc31742235cd0f60d6d03ac502ed5d822
Mike Ball <michaelhball@gmail.com>
2014-05-30 19:37:37 -0700

    Version Upgrades

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mockup/lein-template "0.1.0"
+(defproject mockup/lein-template "0.1.1-SNAPSHOT"
   :description "Easily create clickable wireframe prototypes"
   
   :url "https://github.com/mikeball/mockup"
@@ -7,7 +7,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.reader "0.8.3"]
-                 [ring-server "0.2.8"]
-                 [ring-refresh "0.1.1"]
+                 [org.clojure/tools.reader "0.8.4"]
+                 [ring-server "0.3.1"]
+                 [ring-refresh "0.1.2"]
                  [org.taoclj/tao-mustache "0.0.2"]])
