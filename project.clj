931ea560c14772f5b1adfe1e85b81eef3bde5eae
Michael Ball <michaelhball@gmail.com>
2017-02-05 20:55:31 -0800

    Update deps

--- project.clj
+++ project.clj
@@ -6,8 +6,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.reader "0.8.4"]
-                 [ring-server "0.3.1"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
+                 [org.clojure/tools.reader "1.0.0-beta4"]
+                 [ring-server "0.4.0"]
                  [ring-refresh "0.1.2"]
                  [org.taoclj/tao-mustache "0.1.0"]])
